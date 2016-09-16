f95b5d2a6459162ad6b8555a18f7f35131f6083b
samuel kamau <sam@ralphowino.com>
2016-09-16 08:52:29 +0300

    Initial setup

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests, Helpers;
+
+    /**
+     * The controller's default status code
+     *
+     * @var int
+     */
+    protected $statusCode = \Dingo\Api\Http\Response::HTTP_OK;
+
+    /**
+     * Validate the controller input
+     *
+     * @param $action
+     */
+    public function validateInput($action)
+    {
+        if ($action == 'create')
+            $rules = $this->create_rules;
+        else
+            $rules = $this->update_rules;
+        $this->validateRequest($rules);
+    }
+
+    /**
+     * Handles the actual request validation
+     *
+     * @param array $rules
+     * @param array $messages
+     * @param array $customAttributes
+     * @return mixed
+     */
+    public function validateRequest(array $rules, array $messages = [], array $customAttributes = [])
+    {
+        $request = app('request');
+        $validator = app('validator')->make($request->all(), $rules, $messages, $customAttributes);
+        if ($validator->fails()) {
+            return $this->returnValidationErrors($validator);
+        }
+    }
+
+    /**
+     * Returns the validation errors
+     *
+     * @param $validator
+     * @return mixed
+     */
+    public function returnValidationErrors($validator)
+    {
+        return $this->response()->array([
+            'message'   => 'Validation error',
+            'errors'        => $validator->errors()
+        ])->setStatusCode(422);
+    }
+}
